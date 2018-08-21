4cb333ad2735b6368826bb8cff896792b0a2067f
Vinod <vkumar201415@gmail.com>
2018-08-21 08:54:34 +0530

    Added build essential

--- setup/core.sh
+++ setup/core.sh
@@ -46,6 +46,8 @@ sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install jq git vsftpd aptitude apache2-utils php7.0-curl sqlite3 libsqlite3-dev python-dev gcc python-pip
 sudo pip install --upgrade pip
 
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+
 wget https://files.pythonhosted.org/packages/60/db/645aa9af249f059cc3a368b118de33889219e0362141e75d4eaf6f80f163/pycrypto-2.6.1.tar.gz
 tar -xvzf pycrypto-2.6.1.tar.gz
 cd pycrypto*
